9b038e50142037df070699671e95477e3aa0555c
lyujie-xm <920412430@qq.com>
2023-05-28 04:26:23 +0800

    update repo

--- update.sh
+++ update.sh
@@ -1,5 +1,24 @@
-#!/bin/bash
-dpkg-scanpackages -m ./debs > ./Packages
-bzip2 -fks ./Packages
+#!/bin/sh
+dpkg-scanpackages --multiversion rootfull > Packages
+dpkg-scanpackages --multiversion rootless >> Packages
 
-echo "生成成功！"
+cat Packages | xz > Packages.xz
+cat Packages | bzip2 > Packages.bz2
+cat Packages | gzip > Packages.gz
+cat Packages | lzma > Packages.lzma
+cat Packages | zstd > Packages.zst
+
+apt-ftparchive\
+ -o APT::FTPArchive::Release::Origin="轻悦"\
+ -o APT::FTPArchive::Release::Label="轻悦"\
+ -o APT::FTPArchive::Release::Suite="stable"\
+ -o APT::FTPArchive::Release::Version="1.0"\
+ -o APT::FTPArchive::Release::Codename="ios"\
+ -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64"\
+ -o APT::FTPArchive::Release::Components="main"\
+ -o APT::FTPArchive::Release::Description="轻悦"\
+ release . > Release
+
+git add .
+git commit -m "update repo"
+git push
