577c621794341f2af7a769b620546d497f529fd9
lyujie-xm <920412430@qq.com>
2023-05-28 20:42:24 +0800

    .

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
-02dpkg-scanpackages -m ./debs > Packages
-bzip2 -c Packages > Packages.bz2
-gzip -c Packages > Packages.gz
+#!/bin/bash
+# dpkg-scanpackages -m ./debs > ./Packages
+# bzip2 -fks ./Packages
 
-cd beta
+# echo "生成成功！"
 
-dpkg-scanpackages -m ./debs > Packages
-bzip2 -c Packages > Packages.bz2
-gzip -c Packages > Packages.gz
 
-cd ..
+echo "搜索.DS_Store文件并刪除它們"
+find ./ -iname ".DS_Store" -exec rm {}  \;
+echo ".DS_Store 文件已刪除"
+echo "开始重新压缩deb"
+dpkg-scanpackages -m . /dev/null >Packages
+echo "完成压缩deb"
+rm ./Packages.bz2
+echo "刪除Paackages.bz2"
+bzip2 -fks Packages
+echo "重新产生Paackages.bz2完成"
